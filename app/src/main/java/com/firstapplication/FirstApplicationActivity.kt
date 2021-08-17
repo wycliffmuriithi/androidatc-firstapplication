232c3ba9e10e27b98a25af462af16b8afc816810
kipkem <kipkemoi08@gmail.com>
2021-08-17 09:29:03 +0300

    android atc training

---
+++ app/src/main/java/com/firstapplication/FirstApplicationActivity.kt
@@ -0,0 +1,11 @@
+package com.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstApplicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firstapplication)
+    }
+}
---
+++ app/src/main/java/com/firstapplication/FirstApplicationSecondActivity.kt
@@ -0,0 +1,11 @@
+package com.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstApplicationSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_application_second)
+    }
+}
